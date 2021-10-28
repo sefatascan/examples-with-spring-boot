85ae35f556e785c6d380c5f47eeb1817e3064654
Sefa Tascan <sefatascan@hotmail.com>
2021-10-28 22:58:43 +0300

    init: created app

---
+++ src/main/java/com/sefa/examples/ExamplesWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sefa.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplesWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamplesWithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sefa/examples/ExamplesWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sefa.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
