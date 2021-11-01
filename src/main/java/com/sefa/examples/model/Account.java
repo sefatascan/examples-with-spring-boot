b244e22ab96fd7ec982f95e12d46ae16dd10863e
Sefa Tascan <sefatascan@hotmail.com>
2021-11-01 23:50:35 +0300

    feat: #1 added models and some config

---
+++ src/main/java/com/sefa/examples/model/Account.java
@@ -0,0 +1,37 @@
+package com.sefa.examples.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Builder
+@NoArgsConstructor
+@Data
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    private BigDecimal balance;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @OneToMany(mappedBy = "account", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Transaction> transactions;
+
+}
---
+++ src/main/java/com/sefa/examples/model/Transaction.java
@@ -0,0 +1,35 @@
+package com.sefa.examples.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@Data
+@Entity
+public class Transaction {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    @CreatedDate
+    private LocalDateTime transactionDate;
+
+    private BigDecimal amount;
+
+    private TransactionEnum transactionType;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+}
